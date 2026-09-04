97cd20a261fc9315218f132745268b24593fa27a
Damir <contact.damir.1@gmail.com>
2026-09-04 04:29:27 +0100

    pin sanity studio deploy target

--- my-blog/sanity.cli.js
+++ my-blog/sanity.cli.js
@@ -11,5 +11,8 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    // Pins deploys to the existing damirbuilds.sanity.studio hostname so
+    // `npm run deploy` stops prompting to pick/create a hostname each time.
+    appId: 'qky0newzst7c5qss7k0fb3ts',
   },
 })
