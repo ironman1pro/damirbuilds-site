7e6331b8b179f1737b71f4241cd839ae1d0b01c1
Damir <contact.damir.1@gmail.com>
2026-09-17 01:10:07 +0100

    Remove AI Cold Calling from nav for now

--- static-site/site-nav.js
+++ static-site/site-nav.js
@@ -34,7 +34,6 @@
             '<span class="caret" aria-hidden="true"></span>' +
           '</button>' +
           '<div class="dropdown-menu">' +
-            '<a href="/ai-cold-calling.html">AI Cold Calling</a>' +
             '<a href="/speed-to-lead.html">Speed-to-Lead System</a>' +
           '</div>' +
         '</div>' +
